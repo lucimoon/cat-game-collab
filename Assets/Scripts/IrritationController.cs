80f4eb44d27fb0883de809e0a53b3e06008ce66c
Genevieve Curry <gcurry@protonmail.com>
2021-08-21 10:40:22 -0700

    Preliminary setup of irritation system.

---
+++ Assets/Scripts/IrritationController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IrritationController : MonoBehaviour
+{
+
+  // Todo: Replace with more flexible irritation handler
+  public int IrritationCount1 = 0;
+  public int IrritationCount2 = 0;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
