6f539c7bccdd2b6d886a77338885f9d2f54d67fe
Genevieve Curry <gcurry@protonmail.com>
2021-08-23 10:26:43 -0700

    Saving scripts and settings files

--- Assets/Scripts/NonPlayerController.cs
+++ Assets/Scripts/NonPlayerController.cs
@@ -13,24 +13,45 @@ public class NonPlayerController : MonoBehaviour
   public IrritationBar irritationBar;
 
   // Declare reference variables
-  CharacterController characterController;
-  Color color;
+  Material npcMaterial;
 
   void Start()
   {
     irritationScore = startingIrritation;
+    npcMaterial = GetComponent<Renderer>().material;
   }
 
   // UpdateIrritation triggered by InteractableController
   private void UpdateIrritation(float irritationModifier)
   {
     float newIrritationScore = irritationScore + irritationModifier;
+    // colors for testing
+    Color red = new Color(0.6981f, 0.5160248f, 0.4625756f, 1f);
+    Color green = new Color(0.494838f, 0.754717f, 0.6536991f, 1f);
+    Color gray = new Color(0.6981f, 0.6981f, 0.6981f, 1f);
 
+    // flash colors for testing
+    if (newIrritationScore > irritationScore)
+    {
+      StartCoroutine(ResetMaterial(red, gray));
+    }
+    else if (newIrritationScore < irritationScore)
+    {
+      StartCoroutine(ResetMaterial(green, gray));
+    }
+
+    // Update score if within threshold
     if (newIrritationScore >= minIrritation && newIrritationScore <= maxIrritation)
     {
       irritationScore += irritationModifier;
       irritationBar.UpdateIrritationBar();
     }
+  }
 
+  IEnumerator ResetMaterial(Color startColor, Color resetColor)
+  {
+    npcMaterial.color = startColor;
+    yield return new WaitForSeconds(1);
+    npcMaterial.color = resetColor;
   }
 }
