a98ae21e1567b886099745ae8d788ff0ec5d9558
Genevieve Curry <gcurry@protonmail.com>
2021-08-29 16:10:26 -0700

    Update controllers to add tooltip functionality, also messed with tabs and set them to 2. Eek.

--- Assets/Scripts/InteractableController.cs
+++ Assets/Scripts/InteractableController.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using TMPro;
 
 public class InteractableController : MonoBehaviour
 {
+  // Prefab reference
+  public Canvas interactionTip;
+  public string interactionTipText;
+  public bool allowMultipleInteractions = false;
+
   [System.Serializable]
   public struct IrritationReaction
   {
@@ -12,21 +17,64 @@ public class InteractableController : MonoBehaviour
     public float irritationModifier;
   }
   public List<IrritationReaction> irritationReactionList = new List<IrritationReaction>();
+  Canvas interactionTipInstance;
 
-  // Sample trigger
-  private void OnTriggerEnter(Collider other)
+  void Awake()
   {
-    if (other.gameObject.CompareTag("Player"))
+    initInteractionTip();
+  }
+
+  void initInteractionTip()
+  {
+    if (interactionTip != null)
     {
-      foreach (IrritationReaction irritation in irritationReactionList)
-      {
-        handleUpdateIrritation(irritation.nonPlayerController, irritation.irritationModifier);
-      }
+      // Set position of interaction tool tip (attempt here to take into account size of object + camera position)
+      Quaternion rotation = Camera.main.transform.rotation;
+      Vector3 position = gameObject.transform.position;
+      position.y = gameObject.transform.localScale.y + position.y;
+      position.x = gameObject.transform.localScale.x + position.x;
+      position.z = gameObject.transform.localScale.z + position.z;
+
+      // Create new instance of tool tip based on prefab
+      interactionTipInstance = Instantiate(interactionTip, position, rotation);
+      interactionTipInstance.gameObject.SetActive(false);
+
+      SetInteractionTipText();
+    }
+  }
+
+  // Set the instructional interaction tool tip text
+  void SetInteractionTipText()
+  {
+    TextMeshProUGUI tipDisplayText = interactionTipInstance.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+    tipDisplayText.text = interactionTipText;
+  }
+
+  public void ShowInteractionTip(bool isVisible)
+  {
+    if (interactionTipInstance != null)
+    {
+      interactionTipInstance.gameObject.SetActive(isVisible);
+    }
+  }
+
+  public void HandleInteraction()
+  {
+    // Loop through each attached NPC irritation reaction
+    foreach (IrritationReaction irritation in irritationReactionList)
+    {
+      handleUpdateIrritation(irritation.nonPlayerController, irritation.irritationModifier);
+    }
+    // Set the game object to inactive & destroy the interaction tool tip
+    if (!allowMultipleInteractions)
+    {
+      gameObject.SetActive(false);
+      Destroy(interactionTipInstance.gameObject);
     }
   }
 
   // Uses BroadcastMessage to send irritation to specific NPC
-  private void handleUpdateIrritation(NonPlayerController nonPlayerController, float irritationModifier)
+  public void handleUpdateIrritation(NonPlayerController nonPlayerController, float irritationModifier)
   {
     nonPlayerController.BroadcastMessage("UpdateIrritation", irritationModifier);
   }
