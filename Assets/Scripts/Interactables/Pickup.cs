22c0760c71bf45e63e7eedc3aade46e52e6520fb
Gabbi Gonzalez <gabxgonz@gmail.com>
2021-10-30 17:03:32 -0700

    Refactor Pickup class

--- Assets/Scripts/Interactables/Pickup.cs
+++ Assets/Scripts/Interactables/Pickup.cs
@@ -5,40 +5,58 @@ using UnityEngine;
 #nullable enable
 public class Pickup : Interactable
 {
+  [SerializeField] private Transform? mouthAttachmentPoint;
+  private PlayerAnimation DETATCH_ANIMATION = PlayerAnimation.None;
+  private PlayerAnimation ATTACH_ANIMATION = PlayerAnimation.None;
   private bool isHeld = false;
+  private new Rigidbody? rigidbody;
+
+  protected override void Start()
+  {
+    base.Start();
+    rigidbody = GetComponent<Rigidbody>();
+  }
 
   protected override void MouthInteraction(Transform? attachmentPoint)
   {
     if (isHeld)
     {
-      // Enable Gravity
-      Rigidbody rigidbody = GetComponent<Rigidbody>();
-      rigidbody.useGravity = true;
-      rigidbody.isKinematic = false;
-
-      // Parent to player
-      transform.SetParent(null);
-      this.playerAnimations.Mouth = PlayerAnimation.None;
-
-      isHeld = false;
+      Detatch();
     }
     else
     {
-      if (attachmentPoint != null)
-      {
-        this.playerAnimations.Mouth = PlayerAnimation.Eat;
-        //Disable Gravity
-        Rigidbody rigidbody = GetComponent<Rigidbody>();
-        rigidbody.useGravity = false;
-        rigidbody.isKinematic = true;
-
-        // Parent to player
-        transform.SetParent(attachmentPoint);
-        //Attach to mouth
-        transform.position = attachmentPoint.position;
-
-        isHeld = true;
-      }
+      Attach(attachmentPoint);
     }
+  }
+
+  private void Detatch()
+  {
+    isHeld = false;
+
+    if (rigidbody != null)
+    {
+      rigidbody.useGravity = true;
+      rigidbody.isKinematic = false;
+    }
+
+    transform.SetParent(null);
+
+    this.playerAnimations.Mouth = DETATCH_ANIMATION;
+  }
+
+  private void Attach(Transform? attachmentPoint)
+  {
+    if (rigidbody == null) throw new System.Exception("No Rigidbody attached to Pickup");
+    if (attachmentPoint == null) throw new System.Exception("No attachment point given to Pickup");
+
+    isHeld = true;
+
+    rigidbody.useGravity = false;
+    rigidbody.isKinematic = true;
+
+    this.playerAnimations.Mouth = ATTACH_ANIMATION;
+
+    transform.SetParent(attachmentPoint);
+    transform.position = attachmentPoint.position;
   }
 }
